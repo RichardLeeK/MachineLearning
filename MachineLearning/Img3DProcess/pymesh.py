a8a8950fd3f1974e6e975f7f180d89c216017061
CNM <CNM@SERVER>
2017-07-19 10:17:47 +0900

    0719

---
+++ MachineLearning/Img3DProcess/pymesh.py
@@ -0,0 +1,4 @@
+import pymesh
+
+mesh = pymesh.load_mesh("D:/Richard/plateau/ml approach/sample/brain.ply");
+
---
+++ MachineLearning/SignalImage/cnn_classifier.py
@@ -0,0 +1,23 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adadelta
+class CNN():
+  def __init__(self, ):
+    self.img_dim = img_dim
+    self.nb_class = nb_class
+
+  def create_mode(img_dim=128, nb_class=9):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_dim, img_dim, 3)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_class, activation='softmax'))
+    model.compile(loss=categorical_crossentropy, optimizer=Adadelta(), metrics=['accuracy'])
+    return model
+  
