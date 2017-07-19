4467234e94edea035137a5e944dfeb877010348d
Richard.Lee <koreateam23@naver.com>
2017-07-19 10:47:57 +0900

    Revert "0719"
    This reverts commit a8a8950fd3f1974e6e975f7f180d89c216017061.

--- MachineLearning/SignalImage/process.py
+++
@@ -1,8 +0,0 @@
-def gos_loader():
-  file = open('rubbish/GOS.csv')
-  lines = file.readlines()
-  gos_map = {}
-  for line in lines[1:]:
-    sl = line.split(',')
-    gos_map[int(sl[0])] = int(sl[1])
-    return gos_map
